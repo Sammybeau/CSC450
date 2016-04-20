3947ca78c55c288fb1fa831773edde6111306c97
sammybeau <Samuel.Miller@cuw.edu>
2016-04-20 20:06:12 +0000

    assignment 10

--- chat/chatclient/chatClient.c
+++ chat/chatclient/chatClient.c
@@ -27,11 +27,11 @@ int main(int argc, char** argv)
     {
         puts("We are Connected");
         //Receive a reply from the server
-        
-        char* server_reply = malloc(2000 * sizeof(char));
+        int MAX_SIZE = 2000 * sizeof(char);
+        char* server_reply = malloc(MAX_SIZE);
         while(1)
         {
-            error = recv(sockfd, server_reply, sizeof(server_reply), 0);
+            error = recv(sockfd, server_reply, MAX_SIZE, 0);
             if(error < 0)
             {
                 puts("recv failed");
