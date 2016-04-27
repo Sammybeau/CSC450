ac827e02c5f7981ad73e7939946d87c436ccd09d
sammybeau <Samuel.Miller@cuw.edu>
2016-04-27 19:44:36 +0000

    assignment 11

---
+++ chat/chatserver/chatTest.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char** argv)
+{
+    char* s = "lol msg 4 hello";
+    char* s2 = "world";
+    int compare = strcmp("wound", s2);
+    printf("here:%i", compare);
+}
